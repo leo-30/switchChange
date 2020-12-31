3f94fda96f701b8732e29f5d135a1303585f5e6f
Mio Harada <mio.rabbit971.snow@gmail.com>
2020-12-31 16:00:31 +0900

    Initial Commit

---
+++ switchChange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  switchChange
+//
+//  Created by Mio Harada on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
